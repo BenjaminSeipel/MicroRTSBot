66c474cb65693d1d17bea475bb431bbd3eadf3e1
Benni <Benjamin.seipel@live.com>
2023-06-15 09:22:25 +0200

    added predator bot

---
+++ src/Predator.java
@@ -0,0 +1,149 @@
+import ai.core.AI;
+import ai.core.AIWithComputationBudget;
+import ai.core.ParameterSpecification;
+import rts.*;
+import rts.units.Unit;
+import rts.units.UnitType;
+import rts.units.UnitTypeTable;
+import util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Predator extends AIWithComputationBudget {
+
+    UnitTypeTable m_utt;
+    //int counter = 0;
+    //int counter1 = 0;
+    //This is the default constructor that microRTS will call:
+
+    public Predator(UnitTypeTable utt) {
+
+        super(100, 100);
+
+        m_utt = utt;
+
+    }
+
+    // This will be called by microRTS when it wants to create new instances of this bot (e.g., to play multiple games).
+
+    public AI clone() {
+
+        return new Predator(m_utt);
+
+    }
+
+
+
+    // This will be called once at the beginning of each new game:    
+
+    public void reset() {
+
+    }
+
+    // Called by microRTS at each game cycle.
+
+    // Returns the action the bot wants to execute.
+
+    public PlayerAction getAction(int player, GameState gs) {
+        try {
+
+            if (!gs.canExecuteAnyAction(player)) return new PlayerAction();
+
+            PhysicalGameState pgs = gs.getPhysicalGameState();
+            PlayerActionGenerator pag = new PlayerActionGenerator(gs, player);
+            PlayerAction pa = new PlayerAction();
+
+            // Gets all Choices from Generator
+            List<Pair<Unit,List<UnitAction>>> unitChoices = pag.getChoices();
+
+            boolean consistent = false;
+            do {
+                // Iterates through all units from the player that can perform an action
+                for(Pair<Unit,List<UnitAction>> selectedUnitWithActions: unitChoices){
+                    Unit selectedUnit = selectedUnitWithActions.m_a;
+                    List<UnitAction>  actionsOfSelectedUnit = selectedUnitWithActions.m_b;
+                    UnitType type = selectedUnit.getType();
+                    {
+                        switch(type.name){
+                            case "Base":
+                                // 3 is just an index
+                               UnitAction unitAction = actionsOfSelectedUnit.get(3);
+                                break;
+                            case "Barracks":
+                                break;
+                            case "Worker":
+                                break;
+                            case "Light":
+                                break;
+                            case "Heavy":
+                                break;
+                            case "Ranged":
+                                break;
+                        }
+                    }
+                }
+
+                Unit base = unitChoices.get(0).m_a;
+
+                UnitAction unitAction = unitChoices.get(0).m_b.get(3);
+
+                ResourceUsage r2 = unitAction.resourceUsage(base, pgs);
+
+                if (pa.getResourceUsage().consistentWith(r2, gs)) {
+
+                    pa.getResourceUsage().merge(r2);
+                    pa.addUnitAction(base, unitAction);
+                    /*
+                    switch(positionOfUnit){
+                        case 238:
+                            if(counter ==0) {
+                                pa.addUnitAction(worker, unitAction);
+                                counter++;
+                            }else {
+                                pa.addUnitAction(worker, unitAction2);
+                                counter--;
+                            }
+                            break;
+                        case 222:
+                            if(counter1==0){
+                                pa.addUnitAction(worker, unitAction4);
+                                counter1++;
+                            }else{
+                                pa.addUnitAction(worker, unitAction3);
+                                counter1--;
+                            }
+                            break;
+                    }
+                    */
+                    consistent = true;
+                }
+
+            } while (!consistent);
+
+            return pa;
+        }catch(Exception e) {
+            // The only way the player action generator returns an exception is if there are no units that
+            // can execute actions, in this case, just return an empty action:
+            // However, this should never happen, since we are checking for this at the beginning
+            return new PlayerAction();
+        }
+    }
+
+
+
+    // This will be called by the microRTS GUI to get the
+
+    // list of parameters that this bot wants exposed
+
+    // in the GUI.
+
+    public List<ParameterSpecification> getParameters()
+
+    {
+
+        return new ArrayList<>();
+
+    }
+
+}
