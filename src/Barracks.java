3ec4ea3f364485724f3fbf6e6c59c36522e138af
Slavko-B <s.bezjak@web.de>
2023-06-30 07:33:21 +0200

    added biased unit production + new heavy logic (not finished)

--- src/Barracks.java
+++ src/Barracks.java
@@ -16,7 +16,7 @@ public class Barracks extends BaseUnit {
         super(unit, actions, pgs, player);
     }
 
-    //Generally just procuce light units
+    //Generally just produce light units
     public UnitAction getNextUnitAction() {
         Iterator<UnitAction> iter = this.actions.iterator();
         UnitAction action = null;
@@ -42,11 +42,39 @@ public class Barracks extends BaseUnit {
     }
 
     //Produce only heavy units for now, maybe produce to a maximum cap of heavy units?
-    public static String getNextUnitToProduce() {
-        return UNIT_LIGHT;
-        /*
+    public String getNextUnitToProduce() {
+        //return UNIT_HEAVY;
+        //calc amount of heavies needed to build diagonal line
+        int maxAmountOfHeavy;
+        if(this.pgs.getHeight() <= 16) {
+            maxAmountOfHeavy = this.pgs.getHeight() / 2;
+        } else {
+            maxAmountOfHeavy = (this.pgs.getHeight() / 3) + 1;
+        }
         Random random = new Random();
-        int randomInt = random.nextInt(3);
+        int randomInt = random.nextInt(6);
+
+        if(getAmountOfHeavies() < maxAmountOfHeavy) {
+            if(randomInt < 2) {
+                lastUnitProduced = UNIT_LIGHT;
+                return UNIT_LIGHT;
+            } else if(randomInt == 2) {
+                lastUnitProduced = UNIT_RANGED;
+                return UNIT_RANGED;
+            } else {
+                lastUnitProduced = UNIT_HEAVY;
+                return UNIT_HEAVY;
+            }
+        } else {
+            if(randomInt < 3) {
+                lastUnitProduced = UNIT_LIGHT;
+                return UNIT_LIGHT;
+            } else {
+                lastUnitProduced = UNIT_RANGED;
+                return UNIT_RANGED;
+            }
+        }
+/*
         if(randomInt == 0){
             lastUnitProduced = UNIT_HEAVY;
             return UNIT_HEAVY;
@@ -56,7 +84,22 @@ public class Barracks extends BaseUnit {
         }else{
             lastUnitProduced = UNIT_RANGED;
             return UNIT_RANGED;
-        }*/
+        }
+ */
+    }
+
+    public int getAmountOfHeavies() {
+        List<Unit> allUnitsInGame = this.pgs.getUnits();
+        Iterator iter = allUnitsInGame.iterator();
+        int counter = 0;
+        Unit unit;
+        while (iter.hasNext()) {
+            unit = (Unit) iter.next();
+            if (unit.getPlayer() == this.player && unit.getType().name == UNIT_HEAVY) {
+                counter++;
+            }
+        }
+        return counter;
     }
 
 }
--- src/Heavy.java
+++ src/Heavy.java
@@ -19,19 +19,16 @@ public class Heavy extends BaseUnit {
         Iterator<UnitAction> iter = this.actions.iterator();
         UnitAction action = null;
 
+        int[] board = {this.pgs.getWidth(), this.pgs.getHeight()};
+        int[] basePosition = getUnitPosition(UNIT_BASE);
+        int[] position = new int[2];
+        int[] defendingPos = new int[2];
+        int[] enemyBase = getEnemyUnitPosition(UNIT_BASE);
+
         while (iter.hasNext()) {
             action = iter.next();
-            int[] positionEnemy = getEnemyUnitPosition(UNIT_BASE);
-            if (positionEnemy == null) {
-                positionEnemy = getEnemyUnitPosition(UNIT_WORKER);
-            }
-            Random rand = new Random();
-            int takeX = rand.nextInt(2);
-            int enemyX = positionEnemy[0];
-            int enemyY = positionEnemy[1];
-
             // Check if there are any enemies within the base radius
-
+/*
             if (isEnemyWithinBaseRadius()) {
                 // Attack the enemy
                 int[] enemyPos = getEnemyNextToMe();
@@ -44,7 +41,15 @@ public class Heavy extends BaseUnit {
                     return this.getAttack().get(0);
                 }
                 if (enemyPos != null) {
-                    return this.moveToPosition(enemyPos[0], enemyPos[1]);
+                    if (this.unit.getX() < enemyPos[0] && !this.getMoveRight().isEmpty()) {
+                        return this.getMoveRight().get(0);
+                    } else if (this.unit.getX() > enemyPos[0] && !this.getMoveLeft().isEmpty()) {
+                        return this.getMoveLeft().get(0);
+                    } else if (this.unit.getY() < enemyPos[1] && !this.getMoveDown().isEmpty()) {
+                        return this.getMoveDown().get(0);
+                    } else if (this.unit.getY() > enemyPos[1] && !this.getMoveUp().isEmpty()) {
+                        return this.getMoveUp().get(0);
+                    }
                 }
             } else {
                 // Move back to the base
@@ -52,14 +57,83 @@ public class Heavy extends BaseUnit {
                 int baseX = basePosition[0];
                 int baseY = basePosition[1];
 
-                return moveToPosition(baseX, baseY);
+                if (this.unit.getX() > baseX && !this.getMoveLeft().isEmpty()) {
+                    return this.getMoveLeft().get(0);
+                } else if (this.unit.getX() < baseX && !this.getMoveRight().isEmpty()) {
+                    return this.getMoveRight().get(0);
+                } else if (this.unit.getY() > baseY && !this.getMoveUp().isEmpty()) {
+                    return this.getMoveUp().get(0);
+                } else if (this.unit.getY() < baseY && !this.getMoveDown().isEmpty()) {
+                    return this.getMoveDown().get(0);
+                }
+            }
+*/
+            if (basePosition[0] < enemyBase[0] && basePosition[1] < enemyBase[1]) {
+                if (board[0] <= 16 && board[1] <= 16) {
+                    position[0] = board[0] / 2 + 1;
+                    position[1] = board[1] / 2 + 1;
+                } else {
+                    position[0] = board[0] / 3;
+                    position[1] = board[1] / 3;
+                }
+                for (int width = 0; width < position[0]; width += 2) {
+                    int height = position[1] - width - 1;
+                    if (this.unit.getX() == width && this.unit.getY() == height) {
+                        return this.waitAction().get(0);
+                    }
+                    if (this.pgs.getUnitAt(width, height) == null) {
+                        defendingPos[0] = width;
+                        defendingPos[1] = height;
+                        break;
+                    }
+                }
+
+            } else {
+                //TODO heavies dont set up the right way when bot in lower right corner
+                if (board[0] > 16 && board[1] > 16) {
+                    position[0] = board[0] - (board[0] / 2 + 1);
+                    position[1] = board[1] - (board[1] / 2 + 1);
+                } else {
+                    position[0] = board[0] - (board[0] / 3);
+                    position[1] = board[1] - (board[1] / 3);
+                }
+                for (int width = position[0]; width > 0; width -= 2) {
+                    int height = position[1];
+                    if (this.unit.getX() == width && this.unit.getY() == height) {
+                        return this.waitAction().get(0);
+                    }
+                    if (this.pgs.getUnitAt(width, height) == null) {
+                        defendingPos[0] = width;
+                        defendingPos[1] = height;
+                        break;
+                    }
+                }
+            }
+            //if enemy unit in close proximity of defendingPos -> attack
+            int[] closestEnemy = getClosestEnemyToPos(defendingPos);
+            if(Math.abs(closestEnemy[0]-defendingPos[0]) + Math.abs(closestEnemy[1])-defendingPos[1] <= this.unit.getAttackRange()) {
+                return this.getAttack().get(0);
+            }else if(Math.abs(closestEnemy[0]-defendingPos[0]) + Math.abs(closestEnemy[1])-defendingPos[1] <= 2) {
+                int attackX = Math.abs(closestEnemy[0]-defendingPos[0]);
+                int attackY = Math.abs(closestEnemy[1]-defendingPos[1]);
+                return this.moveToPosition(attackX, attackY);
             }
 
+            return this.moveToPosition(defendingPos[0], defendingPos[1]);
+
+            /*if (this.unit.getX() > baseX && !this.getMoveLeft().isEmpty()) {
+                return this.getMoveLeft().get(0);
+            } else if (this.unit.getX() < baseX && !this.getMoveRight().isEmpty()) {
+                return this.getMoveRight().get(0);
+            } else if (this.unit.getY() > baseY && !this.getMoveUp().isEmpty()) {
+                return this.getMoveUp().get(0);
+            } else if (this.unit.getY() < baseY && !this.getMoveDown().isEmpty()) {
+                return this.getMoveDown().get(0);
+            }
             if (!(this.waitAction().isEmpty())) {
                 return this.waitAction().get(0);
-            }
+            }*/
         }
-
         return new UnitAction(UnitAction.TYPE_NONE);
     }
 
@@ -82,7 +156,23 @@ public class Heavy extends BaseUnit {
         return false;
     }
 
+    private int[] getClosestEnemyToPos(int[] pos) {
+        int[] enemyPos = new int[2];
+        List<Unit> units = this.pgs.getUnits();
+        for(Unit u : units) {
+            if(u.getPlayer() != this.player) {
+                int uX = u.getX();
+                int uY = u.getY();
+                if(Math.abs(uX-pos[0]) + Math.abs(uY-pos[1]) <= Math.abs(enemyPos[0]-pos[0]) + Math.abs(enemyPos[1]-pos[1])) {
+                    enemyPos[0] = uX;
+                    enemyPos[1] = uY;
+                }
+            }
+        }
+        return enemyPos;
+    }
 
+    /*
     private int[] getClosestEnemyWithinProtectionArea() {
         int[] basePosition = getUnitPosition(UNIT_BASE);
         System.out.println("Base Position: " + basePosition[0] + ", " + basePosition[1] + "\n");
@@ -101,5 +191,5 @@ public class Heavy extends BaseUnit {
         }
         return null;
     }
-
+     */
 }
